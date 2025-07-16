f775a7d4ba98442186d2db853a8944f438be8d46
Peter Pulmann <peter.pulmann@beset.sk>
2025-07-16 09:51:27 +0200

    Aby sa dalo v eclipse

---
+++ plugins/transforms/identity/src/main/java/org/apache/hop/pipeline/transforms/identity/IdentityData.java
@@ -0,0 +1,11 @@
+package org.apache.hop.pipeline.transforms.identity;
+
+import org.apache.hop.pipeline.transform.BaseTransformData;
+import org.apache.hop.pipeline.transform.ITransformData;
+
+public class IdentityData extends BaseTransformData implements ITransformData {
+
+  public IdentityData() {
+    super();
+  }
+}
