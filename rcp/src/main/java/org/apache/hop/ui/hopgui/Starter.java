0d1604a38a17292f1831fc96d267383bf509a424
Peter Pulmann <peter.pulmann@beset.sk>
2026-07-16 13:56:39 +0200

    Add BOM fičúriny

--- rcp/src/main/java/org/apache/hop/ui/hopgui/Starter.java
+++ rcp/src/main/java/org/apache/hop/ui/hopgui/Starter.java
@@ -1,12 +1,19 @@
 package org.apache.hop.ui.hopgui;
 
+import java.net.URL;
+
 public class Starter {
 
   public static void main(String[] args) {
 
     System.out.println("--------------------------------------------");
     System.out.println(System.getProperty("org.apache.poi.ss.ignoreMissingFontSystem"));
+    System.out.println(
+        "DHOP_SHARED_JDBC_FOLDERS: " + System.getProperty("HOP_SHARED_JDBC_FOLDERS"));
     System.out.println("===============================================");
+    URL an = Starter.class.getClassLoader().getResource("oracle/jdbc/OracleDriver.class");
+    System.out.println("AN " + an);
+
     HopGui.main(args);
   }
 }
